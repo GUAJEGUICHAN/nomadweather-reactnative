515b9dbfab293e8f024b1adbb936fb214feb9ef2
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-06 19:30:28 +0900

    [구현] 날씨 앱 프토토타입

--- App.js
+++ App.js
@@ -4,9 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>지금 작동중? 오 대박</Text>
-      <Text>개미쳤다!</Text>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <View style={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp} >27</Text>
+          <Text style={styles.description} >Sunny</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +21,40 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flexDirection: "row",
+    backgroundColor: 'tomato',
+    // alignItems: 'flex-end',
+    // justifyContent: 'flex-end',
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    // flex: 1,
+    // alignContent: "flex-end",
+    // justifyContent: "center"
+    fontSize: 68,
+    fontWeight: "500"
+  },
+  weather: {
+    flex: 3,
+    backgroundColor: "teal",
+  },
+  day: {
+    flex: 1,
+    alignContent: "flex-end",
+    // justifyContent: "center",
+    alignItems: "center",
+  },
+
+  temp: {
+    marginTop: 50,
+    fontSize: 178,
+  },
+  description: {
+    marginTop: -30,
+    fontSize: 60
   },
 });
